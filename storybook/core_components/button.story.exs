bc751da9ead46b1aa391e2b3b714c23e3aeb7648
Milad <milad@high-mobility.com>
2025-01-14 18:45:03 +0100

    implement button component

--- storybook/core_components/button.story.exs
+++ storybook/core_components/button.story.exs
@@ -1,29 +1,95 @@
 defmodule Storybook.Components.CoreComponents.Button do
   use PhoenixStorybook.Story, :component
-
   def function, do: &JuntosWeb.CoreComponents.button/1
   def render_source, do: :function
 
   def variations do
     [
       %Variation{
-        id: :default,
+        id: :primary,
+        attributes: %{},
+        slots: [
+          "Primary Large"
+        ]
+      },
+      %Variation{
+        id: :primary_medium,
         attributes: %{
-          type: "button",
-          class: "bg-emerald-400 hover:bg-emerald-500 text-emerald-800"
+          variant: "primary",
+          size: "md",
+          type: "button"
         },
         slots: [
-          "Click me!"
+          "Primary Medium"
         ]
       },
       %Variation{
         id: :disabled,
         attributes: %{
-          type: "button",
+          variant: "primary",
           disabled: true
         },
         slots: [
-          "Click me!"
+          "Primary + Disabled"
+        ]
+      },
+      %Variation{
+        id: :secondary,
+        attributes: %{
+          variant: "secondary"
+        },
+        slots: [
+          "Secondary Large"
+        ]
+      },
+      %Variation{
+        id: :secondary_with_icon,
+        attributes: %{
+          variant: "secondary",
+          icon_right: "hero-arrow-right",
+          icon_left: "hero-arrow-right"
+        },
+        slots: [
+          "Secondary Large"
+        ]
+      },
+      %Variation{
+        id: :icon_only,
+        attributes: %{
+          icon_left: "hero-arrow-right",
+          size: "md"
+        },
+        slots: [
+          ""
+        ]
+      },
+      %Variation{
+        id: :outline,
+        attributes: %{
+          variant: "outline"
+        },
+        slots: [
+          "Outline Large"
+        ]
+      },
+      %Variation{
+        id: :outline_disabled,
+        attributes: %{
+          variant: "outline",
+          disabled: true,
+          size: "md"
+        },
+        slots: [
+          "Outline Disabled"
+        ]
+      },
+      %Variation{
+        id: :link,
+        attributes: %{
+          variant: "link"
+        },
+        slots: [
+          "Link"
         ]
       }
     ]
