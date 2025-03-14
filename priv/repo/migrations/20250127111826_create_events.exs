3b54e9222b7d8de126694df641c1b7e1ac7c8868
Milad Rastian <miladrastian@gmail.com>
2025-03-14 19:05:04 +0100

    fix: increase event description size

--- priv/repo/migrations/20250127111826_create_events.exs
+++ priv/repo/migrations/20250127111826_create_events.exs
@@ -10,7 +10,7 @@ defmodule Juntos.Repo.Migrations.CreateEvents do
       add :end_datetime, :naive_datetime, null: false
       add :time_zone, :string, null: false
       add :slug, :string, null: false
-      add :description, :string
+      add :description, :text
       add :location, :map
 
       add :creator_id, references(:users, type: :binary_id, on_delete: :nilify_all), null: true
