933bfe71e5deac6f04c9d1bf7f5a6684bd6d882a
Milad Rastian <miladrastian@gmail.com>
2025-03-28 13:33:37 +0100

    feat: load default image cover when not image was uploaded

--- lib/juntos_web/live/event_live/show.ex
+++ lib/juntos_web/live/event_live/show.ex
@@ -315,6 +315,14 @@ defmodule JuntosWeb.EventLive.Show do
     """
   end
 
+  defp event_cover_image(%{cover_image: %{original: nil}} = assigns) do
+    ~H"""
+    <picture class="max-w-lg" data-role="default-image-cover">
+      <img src="/images/defaults/covers/01.jpg" class="aspect-square rounded-lg" />
+    </picture>
+    """
+  end
+
   defp event_cover_image(%{cover_image: %{media_type: :gif}} = assigns) do
     ~H"""
     <picture class="max-w-lg">
